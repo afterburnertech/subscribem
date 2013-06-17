b9569908ae09320e3d74fde70933a91375d24c5d
afterburnertech <brandon@afterburnertechnologies.com>
2013-06-17 16:21:37 -0400

    power out lol

--- app/controllers/subscribem/application_controller.rb
+++ app/controllers/subscribem/application_controller.rb
@@ -1,4 +1,10 @@
 module Subscribem
   class ApplicationController < ActionController::Base
+
+  	def current_account
+  		if user_signed_in?
+  			@current_account ||= 
+  		end
+  	end
   end
 end
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -3,9 +3,17 @@ feature 'Accounts' do
 	scenario 'creating an account' do
 		visit subscribem.root_url 
 		click_link 'Account Sign up'
+
 		fill_in 'Name', :with => 'Test'
+		fill_in 'Email', :with => "subscriber@example.com"
+		#makes sure Capybara knows which password field to fill in
+		password_field_id = "account_owner_attributes_password"
+		fill_in 'Password', :with => "password"
+		fill_in 'Password confirmation', :with => "password"
 		click_button 'Create Account'
+
 		success_message = 'Your account has been successfully created.'
 		page.should have_content(success_message)
+		page.should have_content("Signed in as subscriber@example.com")
 	end
 end
