c8e21c95b3f02a615e93e6bf516305a7f979fb4b
afterburnertech <brandon@afterburnertechnologies.com>
2013-06-20 13:55:11 -0400

    subscriptions engine

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Subscribem::Engine.routes.draw do
 	constraints(Subscribem::Constraints::SubdomainRequired) do
 		scope :module => "account" do
 			root :to => "dashboard#index"
-			get 'sign_in', :to => "sessions#new"
+			get '/sign_in', :to => "sessions#new"
 			post '/sign_in', :to => "sessions#create", :as => :sessions
+			get '/sign_up', :to => "users#new", :as => :user_sign_up
+			post '/sign_up', :to => "users#create", :as => :user_sign_up
 		end
 	end
 
