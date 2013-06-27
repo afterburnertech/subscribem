04885cd69ce4dfdd5f4c512475c86585ad7b4322
afterburnertech <brandon@afterburnertechnologies.com>
2013-06-27 13:48:51 -0400

    mid dev

--- app/controllers/subscribem/account/accounts_controller.rb
+++ app/controllers/subscribem/account/accounts_controller.rb
@@ -2,10 +2,20 @@ require_dependency "subscribem/application_controller"
 
 module Subscribem
   class Account::AccountsController < ApplicationController
+  	before_filter :authenticate_user!
+  	before_filter :authorize_owner, :only => [:edit, :update]
+
   	def update
   		if current_account.update_attributes(params[:account])
   			flash[:success] = "Account updated successfully."
+  			if current_account.previous_changes.include?("plan_id")
+  				plan = current_account.plan
+  				flash[:success] += " You are now on the '#{plan.name}' plan."
+  			end
   			redirect_to root_path
+  		else
+  			flash[:error] = "Account could not be updated."
+  			render :edit
   		end
   	end
   end
--- app/controllers/subscribem/application_controller.rb
+++ app/controllers/subscribem/application_controller.rb
@@ -3,7 +3,23 @@ module Subscribem
   class ApplicationController < ::ApplicationController
 
   	#all the code that goes here is extended in 
-  	#app/extenders/controllers/application_controller_decorator.rb
+  	#app/extenders/controllers/application_controller.rb
 
+  	#i'm not sure why this code isn't in the extender
+  	def force_authentication!(user)
+		env['warden'].set_user(user.id, :scope => :user)
+	end
+
+	def owner?
+		current_account.owner?(current_user)
+	end
+	helper_method :owner?
+
+	def authorize_owner
+		unless owner?
+			flash[:error] = "You are not allowed to do that."
+			redirect_to root_path
+		end
+	end
   end
 end
