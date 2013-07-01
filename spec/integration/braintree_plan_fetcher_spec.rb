1461e3796d8d4c7c3f2adaeec7bd844cc8cad386
afterburnertech <brandon@afterburnertechnologies.com>
2013-07-01 13:25:39 -0400

    Revert "Revert "starting subscriptions with braintree""
    This reverts commit f9538fd001e83c850a1ef82f104bb06bd50f131a.

---
+++ spec/integration/braintree_plan_fetcher_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+describe BraintreePlanFetcher do 
+
+	let(:faux_plan) do
+		double('Plan',
+				:id 	=> "faux1",
+				:name 	=> "Starter",
+				:price 	=> "9.95")
+	end
+	it "fetches and stores plans" do
+		Braintree::Plan.should_receive(:all).and_return([faux_plan])
+		Subscribem::Plan.should_receive(:create).with({
+			:braintree_id 	=> "faux1",
+			:name 			=> "Starter",
+			:price			=> "9.95"
+			})
+	BraintreePlanFetcher.store_locally
+	end
+end
